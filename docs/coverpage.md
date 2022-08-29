b7c45166056f3a92789e08c256d182f2aa35dcb9
yaoyang <kiki@smarty.so>
2022-08-29 11:53:24 +0800

    Fork from git@github.com:smarty-kiki/mvc_frame.git and modify project name

--- docs/coverpage.md
+++ docs/coverpage.md
@@ -1,6 +1,6 @@
-# mvc_frame <small>1.0.0</small>
+# team <small>1.0.0</small>
 
 > 系统的能力
 
-[GitHub](https://github.com/smarty-kiki/mvc_frame)
+[GitHub](https://github.com/smarty-kiki/team)
 [开始上手](intro)
--- project/tool/start_development_server.sh
+++ project/tool/start_development_server.sh
@@ -4,14 +4,14 @@ ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../..
 
 sh $ROOT_DIR/project/tool/dep_build.sh link
 
-sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name mvc_frame \
+sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name team \
     -v $ROOT_DIR/../frame:/var/www/frame \
-    -v $ROOT_DIR/:/var/www/mvc_frame \
-    -v $ROOT_DIR/project/config/development/nginx/mvc_frame.conf:/etc/nginx/sites-enabled/default \
-    -v $ROOT_DIR/project/config/development/supervisor/mvc_frame_queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
+    -v $ROOT_DIR/:/var/www/team \
+    -v $ROOT_DIR/project/config/development/nginx/team.conf:/etc/nginx/sites-enabled/default \
+    -v $ROOT_DIR/project/config/development/supervisor/team_queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
     -v $ROOT_DIR/project/config/development/supervisor/queue_job_watch.conf:/etc/supervisor/conf.d/queue_job_watch.conf \
-    -e 'PRJ_HOME=/var/www/mvc_frame' \
+    -e 'PRJ_HOME=/var/www/team' \
     -e 'ENV=development' \
     -e 'TIMEZONE=Asia/Shanghai' \
-    -e 'AFTER_START_SHELL=/var/www/mvc_frame/project/tool/development/after_env_start.sh' \
+    -e 'AFTER_START_SHELL=/var/www/team/project/tool/development/after_env_start.sh' \
 registry.cn-shenzhen.aliyuncs.com/smarty/debian_php_dev_env start
